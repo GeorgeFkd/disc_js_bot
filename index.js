c3f7a6db54ebd1550c267f7474e78709ea2360fe
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2021-03-24 18:50:27 +0200

    Discord Bot Deploy first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const jokes = require('give-me-a-joke');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+process.env.TOKEN = 'ODI0MjczNzQyOTQzMzU0OTAw.YFs-vg._fg_1ayLm7wCtK7l7RuelDeNEVM'
+;
+const TOKEN = process.env.TOKEN;
+
+
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', async msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+    msg.channel.send('pong');
+
+  } else if (msg.content.startsWith('!kick')) {
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    } else {
+      msg.reply('Please tag a valid user!');
+    }
+  }else if(msg.content==='gimme dad joke'){
+    
+    msg.reply("im trying to stop crying bitch here is your damn joke");
+    try{jokes.getRandomDadJoke(function(joke){
+      msg.reply(joke);
+    })
+    }catch(e){
+      msg.reply(`sth went wrong ${e}`);
+    }
+    
+    
+    
+  }else if(msg.content === 'gimme joke'){
+    msg.reply('here it is');
+    try{jokes.getRandomCNJoke(function(joke){
+      msg.reply(joke);
+    })
+    }catch(e){
+      msg.reply(`sth went wrong ${e}`);
+    }
+  }
+  
+});
