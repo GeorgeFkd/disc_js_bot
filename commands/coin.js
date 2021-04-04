4d54a17e358653e150c87a18a8a2531910b4f3db
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2021-04-04 21:39:21 +0300

    Added a coin toss command for heads or tails

---
+++ commands/coin.js
@@ -0,0 +1,13 @@
+
+
+module.exports = {
+    name:"coin",
+    description:"tosses a coin for your decisions",
+    execute(message,args){
+
+        const rand = Math.floor(Math.random()*2 +1);
+        const msg = rand%2===0 ? "Heads" : "Tails";
+        message.reply(msg);
+        // "Κορώνα" : "Γράμματα"
+    }
+}
