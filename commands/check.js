2d420f4f559de126538ca5c2c9d3dfecdb7e2ebe
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2021-04-07 16:31:33 +0300

    tiny commit bcs i forgot a branch

--- commands/check.js
+++ commands/check.js
@@ -9,19 +9,18 @@ module.exports = {
         const thebot = args.pop();
         console.log(message.author.id);
         message.channel.send("this doesnt do anything it is for test purposes")
-        // const therole = message.guild.roles.cache.get(remindmeplsRoleID);
-        // console.log(therole);
-        // console.log("Now the specific stuff");
-        // //TODO THIS ONE WORKS
-        // const forWater = message.guild.members.cache.filter(user=>user._roles.includes(remindmeplsRoleID))
-        // //TODO NA KSESKARTARW AYTA EDW AFOY TA DW LIGO
-        // //console.log(forWater.forEach(guy=>console.log(guy._roles)))
-        // const clowns = message.guild.roles.cache.get(remindmeplsRoleID).members.map(m=>m.user.tag);
-        // console.log(clowns);
-        // console.log(message);
-        // thebot.on('messageReaction',(user)=>{
-        //     console.log(user);
-        // })
+        //TODO ME REACTIONS NA ΕΛΕΓΧΩ ΠΟΙΑ ΚΑΝΑΛΙΑ ΦΑΙΝΟΝΤΑΙ
+        const last  = message.channel.messages.cache.first();
+        console.log(last.id);
+        const guild = thebot.guilds.cache.get(calcoholicsGuildID);
+        //console.log(message.channel.id);
+        thebot.on('messageReactionAdd',(reaction)=>{
+            if(last.id ===reaction.message.id){
+                console.log(reaction)
+                const interestedUsers = Array.from(reaction.users.cache.keys());
+
+            }
+        });
         
     }
 }
