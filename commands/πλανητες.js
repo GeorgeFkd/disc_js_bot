c4f1ee8529eaedf8c2ee64fdc48323423205d049
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2021-04-08 22:32:03 +0300

    The planets are ready to communicate you your innermost truths[just a copy paste really]

---
+++ commands/πλανητες.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const email = require('./email')
+const quotes = [];
+
+module.exports = {
+    name:"πλανητες",
+    description:"Σε ενημερώνει για την ζωή σου δια μέσου των πλανητών,μόνο έγκυρες προβλέψεις",
+    execute(message,args){
+       
+        fs.readFile('predictions.txt','utf-8',(err,data)=>{
+            if(err){
+                console.log(err);
+                message.channel.send("Something went terribly wrong and you should wait");
+                
+                return;
+            }
+            let dataArr = data.split('|')
+            let response = dataArr[Math.floor(Math.random()*dataArr.length)]
+            console.log(response)
+            response = response.replace(/\d{1,}./,'')
+            message.channel.send(response);
+        })
+
+    }
+}
