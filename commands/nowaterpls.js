b4181996a29befee82156294cd86df88dbb80df6
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2021-04-09 09:55:05 +0300

    added an unsub option for the water reminders

---
+++ commands/nowaterpls.js
@@ -0,0 +1,14 @@
+const { execute } = require("./email");
+const { remindmeplsRoleID} = require('../constants');
+
+module.exports = {
+    name:'nowaterpls',
+    description:'unsubscribe from the daily water reminders',
+    execute(message,args){
+
+        const guildMember = message.guild.members.
+        cache.get(message.author.id);
+        await guildMember.roles.remove(remindmeplsRoleID)
+        message.reply("Seems like you can do basic human stuff without being reminded congrats welcome to this cold hard life")
+    }
+}
