c93d256c97fbf4906fb595357ae8549bf86ad0ad
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2021-04-11 18:06:37 +0300

    did some refactoring and reduced drastically the else branching with returns,the center first move is yet to be beaten

--- commands/check.js
+++ commands/check.js
@@ -13,118 +13,77 @@ module.exports = {
     requiredChannels:['playground','testing-ground-v2'],
     requiredRole:'',
     async execute(message,args){
-        const bot = args.pop();
-        console.log(message.channel.id)
-        const theguild = message.guild
-        
-        const thechannel = theguild.channels.cache.get(clownMomentsID);
-        
-        const themessages = thechannel.messages.cache.map(msg=>msg.content);
+        console.log(message.channel.id)        
         const dim = 3;//3+1
-
-        
-
-
-        
         const player = message.author.id
         let finished = false;
-        let boardList = []
+        let boardList = [],botWon,playerWon//holds the tictactoe board
         for(let i =0;i<dim;i++){
             boardList.push([0,0,0])
         }
-        let botWon,playerWon;
         console.log(boardList)
-        let theBoard = drawBoard()
+        let theBoard = drawBoard()//the string representing the board
         message.channel.send(theBoard);
 
-        const filter = (msg)=>{
-            
-            return msg.author.id === player && terms.includes(msg.content.toLowerCase()) 
-        }
-        let isFirstMove,botRow=-1,botCol=-1;
+        const filter = ((msg)=> (msg.author.id === player && terms.includes(msg.content.toLowerCase())))
+        //checks that the player is responding
+        let turns=0;
+        
         while(!finished)
         {       
              
-            message.channel.send('Που βάζεις?(γραμμα κενο αριθμος) ')
-            await message.channel.awaitMessages(filter
-                ,{time:10 * 1000,max:1,errors:['time']})
-                .then(async(collected)=>{
-                    let choice = collected.first().content
-                    console.log('he sent',choice)
-                    
-                    let [row,column] = choice.split(' ')
-                    const rowNum = chars.indexOf(row);
-                    const colNum = Number(column)-1;
-                    if(boardList[rowNum][colNum]===0) {
-                        console.log('the indexes',rowNum,colNum);
-                        console.log(row,column)
-                        theBoard = redrawBoard(theBoard,rowNum,colNum,'x')
-                        boardList[rowNum][colNum] = 1;
-                        //if it is first then it has length 2
-                        //TODO NA ALLAKSEI AYTO TO CONDITION
-                        isFirstMove = theBoard.split('x').length-1===1?true:false
-                        console.log('not the split',isFirstMove)
-                        if(isFirstMove){
-                            [botRow,botCol] = firstMove(rowNum,colNum)
-                        }else{
-                            let ok = checkIfEmptySquare(boardList)
-                            console.log('ok',ok);
-                            if(ok) {
-                                let attack = tictactoeResponse(boardList,0.1);
-                                let def = tictactoeResponse(boardList,1);
-                                console.log('wtf is going on')
-                                console.log(attack,def,boardList)
-                                if(boardList[attack[0]][attack[1]]===0){
-    
-                                    console.log('the attack')
-                                    botRow = attack[0];//ans[0];
-                                    botCol = attack[1];//ans[1];
-                                }else if(boardList[def[0]][def[1]]===0){
-                                    botRow= def[0];//ans[0];
-                                    botCol = def[1]
-                                }else{
-                                    console.log('randomized');
-                                    botRow = Math.floor(Math.random()*3)
-                                    botCol = Math.floor(Math.random()*3)
-                                    
-                                    while(boardList[botRow][botCol]!==0){
-                                        botRow = Math.floor(Math.random()*3)
-                                        botCol = Math.floor(Math.random()*3)
-                                        console.log(botRow,botCol)
-                                    }
-                                }                                                                                
-                                console.log('the choice',botRow,botCol)                            
-                                console.table(boardList);
-                            } else {
-                                finished = true;
-                                return message.channel.send('the game ended and you couldnt beat a helpless bot')
-                            }
-                            
-                        }
-                        boardList[botRow][botCol] = 0.1;
-                        theBoard = redrawBoard(theBoard,botRow,botCol,'o')
-                        botWon= gameFinished(boardList,0.1);
-                        playerWon = gameFinished(boardList,1)
-                        message.channel.send(theBoard);
-
-                        if(playerWon){
-                            finished = true; 
-                            return message.channel.send('Seems like your only one left functioning braincell was enough to beat a mildly retarded bot')
-                        }else if(botWon){
-                            finished = true;
-                            return message.channel.send('you lost to a stupid bot incompetent human being and you are worrying about AI taking your jobs')
-                        }else{
-                            if(!boardList.some(line=>line.includes(0))){                            
-                                message.channel.send('I placed my move');
-                            }
-                        }                                    
-                    }else{ //o xrhsths ebale se tetragwno poy hdh yphrxe
-                        message.channel.send('That is illegal place your move properly thi time');
+        message.channel.send('Που βάζεις?(γραμμα κενο αριθμος) ')
+        await message.channel.awaitMessages(filter
+            ,{time:10 * 1000,max:1,errors:['time']})
+            .then(async(collected)=>{
+                
+                let [row,column] = collected.first().content.split(' ')
+                const rowNum = chars.indexOf(row);
+                const colNum = Number(column)-1;
+                if(boardList[rowNum][colNum]!==0){
+                    await message.channel.send('U blind?')
+                    return;
+                }
+                //here we are with a proper place;
+                turns++;
+                console.log('the indexes',rowNum,colNum);
+                console.log(row,column)
+                theBoard = redrawBoard(theBoard,rowNum,colNum,'x');
+                boardList[rowNum][colNum] = 1;                
+                //TODO NA ALLAKSEI AYTO TO CONDITION
+                if(turns===1){
+                    [botRow,botCol] = firstMove(boardList,rowNum,colNum)
+                }else{
+                    let ok = boardList.some((line)=>line.includes(0))//there exists an empty square
+                    console.log('ok',ok);
+                    if(ok) {//bot plays
+                        ({ botRow, botCol } = botDecision(boardList, botRow, botCol));
+                    } else {
+                        finished = true;
+                        return message.channel.send('the game ended and you couldnt beat a helpless bot')
                     }
-                })
-                .catch(err=>console.log(err))
-            //console.log('here hi hi',choice.content);
-            
+                }   
+                turns++;
+                boardList[botRow][botCol] = 0.1;
+                theBoard = redrawBoard(theBoard,botRow,botCol,'o')
+                botWon= gameFinished(boardList,0.1);
+                playerWon = gameFinished(boardList,1)
+                message.channel.send(theBoard);
+
+                if(playerWon){
+                    finished = true; 
+                    return message.channel.send('Seems like your only one left functioning braincell was enough to beat a mildly retarded bot')
+                }
+                if(botWon){
+                    finished = true;
+                    return message.channel.send('you lost to a stupid bot incompetent human being and you are worrying about AI taking your jobs')
+                }
+                if(!boardList.some(line=>line.includes(0))){                            
+                    return message.channel.send('I placed my move');
+                }
+                                                    
+            })
+            .catch(err=>console.log(err))
         }
         console.log('finished');
         
@@ -132,78 +91,82 @@ module.exports = {
 }
 
 
-
-
 String.prototype.replaceAt = function(index, replacement) {
     return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
 
+function botDecision(boardList, botRow, botCol) {
+    let attack = tictactoeResponse(boardList, 0.1);
+    let def = tictactoeResponse(boardList, 1);
+    console.log('wtf is going on');
+    console.log(attack, def, boardList);
+    
+    if (boardList[attack[0]][attack[1]] === 0) {
+        console.log('the attack');
+        return {botRow:attack[0],botCol:attack[1]}
+    }  
+    if (boardList[def[0]][def[1]] === 0) {
+        console.log('the def')
+        return {botRow:def[0],botCol:def[1]}
+    }  
+    
+    console.log('randomized');
+    do{
+        botRow = Math.floor(Math.random()*3)//[0,1,2]
+        botCol = Math.floor(Math.random()*3)
+    }while(boardList[botRow][botCol]!==0) 
+
+    console.log('the choice', botRow, botCol);
+    console.table(boardList);
+    
+    return { botRow, botCol };
+}
+//nnttt
 function drawBoard(){
     let board= '.  .__    __.__    __.__    __.__    __.\n';
-    //prwth seira ksekina to char index:47 
-    //deyterh seira 80 
-    //trith seira 113
+    //prwth seira index:47 deyterh 80 trith 113
     board+='a|__  ~  __|__  ~  __|__  ~  __|\n'
     board+='b|__  ~  __|__  ~  __|__  ~  __|\n'
     board+='c|__  ~  __|__  ~  __|__  ~  __|\n'
     board+='     1     2     3' 
-    
-    
     return board;
 }
-
+//nnttt 
 function redrawBoard(charboard,rowNum,colNum,char){
     const base = 47;//position of a1 
     const pos = base+rowNum*33+colNum*10
-    console.log('pos',pos)
     charboard = charboard.replaceAt(pos,char)
-   
     return charboard;
-    
-
 }
 
-function checkIfEmptySquare(board){
-    for(let i=0;i<3;i++){
-        for(let j=0;j<3;j++){
-            if(board[i][j]===0){
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-function firstMove(moveCol,moveRow){
+function firstMove(boardList,moveCol,moveRow){
     let isAngle = (moveCol===0 || moveCol ===2) && (moveRow===0 || moveRow ===2)
     if(isAngle){
         return [1,1]//otan bazei gwnia prepei na bazw kentro
-    }else{//ama den balei gwnia den se noiazei pou
-        let i = Math.floor(Math.random()*2)
-        let j = Math.floor(Math.random()*2)
-        return [i,j];
-    }
+    }//ama den balei gwnia den se noiazei pou
+    let botRow,botCol;
+    do{
+        botRow = Math.floor(Math.random()*3)//[0,1,2]
+        botCol = Math.floor(Math.random()*3)
+    }while(boardList[botRow][botCol]!==0) 
+
+    return [botRow,botCol];
+
     
     
 }
-
 function gameFinished(board,val){
     let arr = [0,0,0,0]
-    let totalInLine=0,firstDiag=0,sDiag=0,colSum=0;
+    let [totalInLine,firstDiag,sDiag,colSum] = arr;
     for(let i =0;i<3;i++){
-        //totalInLine = board[i].reduce(add)
         
         firstDiag +=board[i][i];
         sDiag += board[i][board.length-1-i]
         for(let j=0;j<3;j++){
             totalInLine+=board[i][j];
             colSum+=board[j][i];
-            
         }
-        arr[0] = totalInLine;
-        arr[1] = firstDiag;
-        arr[2] = sDiag;
-        arr[3] = colSum
+        arr = [totalInLine,firstDiag,sDiag,colSum];
         console.log(arr)
         if(arr.some(num=>num===val * 3)){
             return true;
@@ -218,9 +181,9 @@ function gameFinished(board,val){
 }
 function tictactoeResponse(board,val){
     let arr = [0,0,0,0]
-    let totalInLine=0,firstDiag=0,sDiag=0,colSum=0;
+    let [totalInLine,firstDiag,sDiag,colSum] = arr;
+    //practically the other way around but irrelevant
     for(let i =0;i<3;i++){
-        //totalInLine = board[i].reduce(add)
         
         firstDiag +=board[i][i];
         sDiag += board[i][board.length-1-i]
@@ -229,59 +192,49 @@ function tictactoeResponse(board,val){
             colSum+=board[j][i];
             
         }
-        arr[0] = totalInLine;
-        arr[1] = firstDiag;
-        arr[2] = sDiag;
-        arr[3] = colSum
+        arr = [totalInLine,firstDiag,sDiag,colSum];
         if(arr.some(num=>num===val * 2)){
             console.log('danger')
             //here we find the danger and just return the indexes
             let danger = arr.indexOf(val * 2);
             console.log('the danger',danger)
-            //im checking if the sum is 2 bcs it means two 1s in somewhere 
+            //im checking if the sum is 2 * value bcs it means two 1s(2*1) in somewhere [without a 0.1]
             //so any 2 occurs is a danger
-            switch(danger){
-                case 0://the i line
-                    //if(!board[i].some(num=> num ===0.1))
-                    
-                    console.log('shiit',i,board[i].indexOf(0))
-                    return [i,board[i].indexOf(0)];
-                    //if this is not true go to the next case
-                case 1:{//the first diag
-                    for(let sq;sq<3;sq++){                        
-                        if(board[sq][sq]===0)//there is an empty spot{
-                            console.log('shit',sq)
-                            return [sq,sq];//case 1                         
-                    }
-                    
-                }
-                case 2:{//the second diag
-                    console.log('s diag sus')
-                    for(let sq=0;sq<3;sq++){   
-                        console.log('ind',sq,2-sq)                     
-                        if(board[2-sq][sq]===0)//there is an empty spot{
-                            console.log('damn',sq)
-                            return [2-sq,sq];//case 1                         
-                    }
-                    break;
-                }
-                case 3:{//the i column
-                    for(let j =0;j<3;j++){
-                        if(board[j][i]===0){
-                            console.log('crap',j,i)
-                            return [j,i];
-                        }
-                    }
-                    break;
-                    
+            if(danger === 0){
+                console.log('shiit',i,board[i].indexOf(0))
+                return [i,board[i].indexOf(0)];
+            }
+
+            if(danger === 1){
+                for(let sq;sq<3;sq++){                        
+                    if(board[sq][sq]===0)//there is an empty spot{
+                        console.log('shit',sq)
+                        return [sq,sq];//case 1                         
                 }
             }
 
+            if(danger === 2){
+                console.log('s diag sus')
+                    for(let sq=0;sq<3;sq++){   
+                        console.log('ind',sq,2-sq)                     
+                        if(board[sq][2-sq]===0)//there is an empty spot{
+                            console.log('damn',sq)
+                            return [sq,2-sq];//case 1                         
+                    }
+            }
+
+            if(danger === 3){
+                for(let j =0;j<3;j++){
+                    if(board[j][i]===0){
+                        console.log('crap',j,i)
+                        return [j,i];
+                    }
+                }
+            }
         }
         totalInLine =0;
         colSum = 0;
 
   }
-  return [2,3];
-
+  return [2,3];//some illegal value so we know to randomize
 }
